f1110e7d40a38f53b52f9112a61a7125a6115da7
hev <git@hev.cc>
2026-07-20 23:08:33 +0800

    assets: imports zhengma tables

--- app/src/main/java/com/osfans/trime/data/base/DataManager.kt
+++ app/src/main/java/com/osfans/trime/data/base/DataManager.kt
@@ -27,6 +27,7 @@ object DataManager {
         schema_list:
           - schema: luna_pinyin
           - schema: luna_pinyin_simp
+          - schema: zhengma
     """
 
     private val lock = ReentrantLock()
