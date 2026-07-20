215b13ec14b935d2bb9004a9813aabaf1ae44b77
hev <git@hev.cc>
2026-07-20 23:09:51 +0800

    style: default to simple.trime

--- app/src/main/java/com/osfans/trime/data/theme/ThemePrefs.kt
+++ app/src/main/java/com/osfans/trime/data/theme/ThemePrefs.kt
@@ -19,7 +19,7 @@ class ThemePrefs(
         string(
             R.string.selected_theme,
             SELECTED_THEME,
-            "trime",
+            "simple.trime",
             R.string.selected_theme_summary,
         )
 
